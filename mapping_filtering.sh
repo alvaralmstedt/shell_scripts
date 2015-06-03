92fc65225232799b86acbfaf40461e270c134713
alvaralmstedt <alvar.almstedt@gmail.com>
2015-06-03 13:41:26 +0200

    doesn't work but works a bit better

--- mapping_filtering.sh
+++ mapping_filtering.sh
@@ -3,13 +3,13 @@
 # Usage: mapping_filter.sh <reference>.fasta <file1>.fastq <file2>.fastq
 # Dependencies: bowtie2, samtools, seqtk
 
-echo "Name of outputdirectory: "
+echo "Pease input desired name of output directory: "
 read OUTDIR
 
 echo `hostname`
 date
 
-BNAME=`date +%C%y_%m_%d`
+DBNAME=`date +%C%y_%m_%d`
 SAM_FULL=mapping_full_$DBNAME.sam
 SAM_MAPPER=mappers_$DBNAME.sam
 SAM_NON_MAPPER=non_mappers_$DBNAME.sam
@@ -19,13 +19,19 @@ LIST_NON_MAPPER=non_mappers_$DBNAME.lst
 echo "Creating bowtie2 database"
 
 # Creates a bowtie2 database and names it by date and a random number
-if [ ! -e $DBNAME ];
-do
-	bowtie2-build -f $1 $DBNAME 
-done
+if [ ! -e $DBNAME ]; then
+
+	bowtie2-build -f $1 $DBNAME
+else
+	echo "DB exists, proceeding..." 
+fi
+
+wait
+
+mkdir $OUTDIR
 
 # Starts bowtie2 mapping
-bowtie2 -x $DBNAME -1 $1 -2 $2 -S $OUTDIR/$SAM_FULL 
+bowtie2 -x $DBNAME -1 $2 -2 $3 -S $OUTDIR/$SAM_FULL 
 
 # Splits the sam files into mappers and non_mappers
 samtools view -S F4 $OUTDIR/$SAM_FULL > $OUTDIR/$SAM_MAPPER &
@@ -36,12 +42,12 @@ wait
 cut -f1 $OUTDIR/$SAM_MAPPER | sort | uniq > $OUTDIR/$LIST_MAPPER &
 cut -f1 $OUTDIR/$SAM_NON_MAPPER | sort | uniq > $OUTDIR/$LIST_NON_MAPPER &
 
-seqtk subseq $1 $OUTIDR/$MAPPER_LIST > $OUTIDR/mappers_$1 &
-seqtk subseq $2 $OUTIDR/$MAPPER_LIST > $OUTIDR/mappers_$2 &
+seqtk subseq $1 $OUTIDR/$MAPPER_LIST > $OUTIDR/mappers_$2 &
+seqtk subseq $2 $OUTIDR/$MAPPER_LIST > $OUTIDR/mappers_$3 &
 wait
 
-seqtk subseq $1 $OUTIDR/$NON_MAPPER_LIST > $OUTIDR/non_mappers_$1 &
-seqtk subseq $2 $OUTIDR/$NON_MAPPER_LIST > $OUTIDR/non_mappers_$2 &
+seqtk subseq $1 $OUTIDR/$NON_MAPPER_LIST > $OUTIDR/non_mappers_$2 &
+seqtk subseq $2 $OUTIDR/$NON_MAPPER_LIST > $OUTIDR/non_mappers_$3 &
 wait
 
 date
