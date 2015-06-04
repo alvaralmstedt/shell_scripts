e1964f768767af0d69d17676d6c75d42bf8addb4
alvaralmstedt <alvar.almstedt@gmail.com>
2015-06-04 14:06:14 +0200

    Added several functionalities

--- mapping_filtering.sh
+++ mapping_filtering.sh
@@ -3,27 +3,29 @@
 # Usage: mapping_filter.sh <reference>.fasta <file1>.fastq <file2>.fastq
 # Dependencies: bowtie2, samtools, seqtk
 
-echo "Pease input desired name of output directory: "
+echo "Pease input desired name of your analysis: "
 read OUTDIR
+
+echo "Do you want the intermediary files to be deleted after completion?(y/n)"
+read DELETE
 
 echo `hostname`
 date
 
-DBNAME=`date +%C%y_%m_%d`
-SAM_FULL=mapping_full_$DBNAME.sam
-SAM_MAPPER=mappers_$DBNAME.sam
-SAM_NON_MAPPER=non_mappers_$DBNAME.sam
-LIST_MAPPER=mappers_$DBNAME.lst
-LIST_NON_MAPPER=non_mappers_$DBNAME.lst
+DATE=`date +%C%y_%m_%d`
+SAM_FULL=mapping_full_$DATE.sam
+SAM_MAPPER=mappers_$DATE.sam
+SAM_NON_MAPPER=non_mappers_$DATE.sam
+LIST_MAPPER=mappers_$DATE.lst
+LIST_NON_MAPPER=non_mappers_$DATE.lst
 
-echo "Creating bowtie2 database"
-
+echo "Creating bowtie2 database..."
 # Creates a bowtie2 database and names it by date and a random number
-if [ ! -e $DBNAME ]; then
+if [ ! -e $1.?.bt2 ]; then
 
-	bowtie2-build -f $1 $DBNAME
+	bowtie2-build -f $1 $1
 else
-	echo "DB exists, proceeding..." 
+	echo "Database aready exists, proceeding..." 
 fi
 
 wait
@@ -31,24 +33,42 @@ wait
 mkdir $OUTDIR
 
 # Starts bowtie2 mapping
-bowtie2 -x $DBNAME -1 $2 -2 $3 -S $OUTDIR/$SAM_FULL 
+echo "Running bowtie2 mapping..."
+bowtie2 -x $1 -1 $2 -2 $3 -S $OUTDIR/$SAM_FULL 
 
 # Splits the sam files into mappers and non_mappers
+echo "Separating sam files..."
 samtools view -S -F4 $OUTDIR/$SAM_FULL > $OUTDIR/$SAM_MAPPER &
 samtools view -S -f4 $OUTDIR/$SAM_FULL > $OUTDIR/$SAM_NON_MAPPER &
 wait
 
-#Makes lists containing the headers of the mapping and non_mapping reads
-cut -f1 $OUTDIR/$SAM_MAPPER | sort | uniq > $OUTDIR/$LIST_MAPPER &
-cut -f1 $OUTDIR/$SAM_NON_MAPPER | sort | uniq > $OUTDIR/$LIST_NON_MAPPER &
+mkdir $OUTDIR/lists
+mkdir $OUTDIR/filtered_reads
 
-seqtk subseq $1 $OUTDIR/$LIST_MAPPER > $OUTDIR/mappers_$2 &
-seqtk subseq $2 $OUTDIR/$LIST_MAPPER > $OUTDIR/mappers_$3 &
+# Makes lists containing the headers of the mapping and non_mapping reads
+echo "Creating lists..."
+cut -f1 $OUTDIR/$SAM_MAPPER | sort | uniq > $OUTDIR/lists/"$OUTDIR"_$LIST_MAPPER &
+cut -f1 $OUTDIR/$SAM_NON_MAPPER | sort | uniq > $OUTDIR/lists/"$OUTDIR"_$LIST_NON_MAPPER &
 wait
 
-seqtk subseq $1 $OUTDIR/$LIST_NON_MAPPER > $OUTDIR/non_mappers_$2 &
-seqtk subseq $2 $OUTDIR/$LIST_NON_MAPPER > $OUTDIR/non_mappers_$3 &
+# Pulling mapped reads from libraries
+echo "Fetching reads..."
+seqtk subseq $2 $OUTDIR/lists/"$OUTDIR"_$LIST_MAPPER > $OUTDIR/filtered_reads/"$OUTDIR"_"$DATE"_mappers_$2 &
+seqtk subseq $3 $OUTDIR/lists/"$OUTDIR"_$LIST_MAPPER > $OUTDIR/filtered_reads/"$OUTDIR"_"$DATE"_mappers_$3 &
 wait
+
+# Pulling non-mapped reads from libraries
+seqtk subseq $2 $OUTDIR/lists/"$OUTDIR"_$LIST_NON_MAPPER > $OUTDIR/filtered_reads/"$OUTDIR"_"$DATE"_non_mappers_$2 &
+seqtk subseq $3 $OUTDIR/lists/"$OUTDIR"_$LIST_NON_MAPPER > $OUTDIR/filtered_reads/"$OUTDIR"_"$DATE"_non_mappers_$3 &
+wait
+
+# Deletes intermediary files if desired
+if [ $DELETE = "y" ] || [ $DELETE = "Y" ]; then
+	echo "Removing intermediary sam files..."
+	rm $OUTDIR/*.sam
+else
+	echo "Keeping sam files"
+fi
 
 date
-echo "Finished running bowtie2 mapping"
+echo "Finished running bowtie2 mapping and filtering"
