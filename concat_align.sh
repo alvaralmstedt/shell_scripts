03c1a802c63627e8669954937a0eb098cafec1cd
Alvar Almstedt <alvar@albiorix.bioenv.gu.se>
2015-10-01 13:49:58 +0200

    still does not work. will write separate python functions to do what I can't do in shell

--- concat_align.sh
+++ concat_align.sh
@@ -23,6 +23,7 @@ CWD=$(pwd -P)
 FILES=$(ls)
 wait
 mkdir named
+echo "named_first loop: "
 for f in $FILES
 do
 	echo "Reformatting header of file: $f" 
@@ -33,7 +34,7 @@ NAMEDFILES=$(cd named && ls)
 wait
 mkdir tabbed
 echo $NAMEDFILES
-
+echo "fasta2tab loop: "
 for nf in $NAMEDFILES
 do
 	echo "Tabbing file: $nf"
@@ -42,31 +43,36 @@ done
 wait
 
 TABBEDFILES=$(cd tabbed && ls)
-
+echo "Prepend loop:"
+# This loop appends the species name to the start of the first sequence in each file
 for spec in $TABBEDFILES
 do
 	echo $SPECIES
 	echo -n "${SPECIES}|gi|" | cat - $CWD/tabbed/$spec | sed '/^$/d' > $CWD/tmp && mv $CWD/tmp $CWD/tabbed/${SPECIES}_${spec}
-	head $CWD/tabbed/${SPECIES}_${spec}
+#	head $CWD/tabbed/${SPECIES}_${spec}
 	rm $CWD/tabbed/$spec
 #	echo "end of specloop"
 #	sleep 1
 #	sed -i '1i/^/$SPECIES gi|;' spec
 done
 
-
+wait
+TABBEDFILES=$(cd tabbed && ls | sort)
 set -- $TABBEDFILES
 wait
 mkdir completed
 echo $1
 sleep 5
-# This loop created the files with the correct species names
+echo "Touch-loop:"
+# This loop creates the files with the correct species names
+# Does not work properly
 for header in $CWD/tabbed/$1
 do
-	echo header
-	FILNAMN=$( ( head $header | cut -f1 -d'gi|' ) )
+	echo "header: "$header
+	FILNAMN=$( ( head $header | cut -f1 -d'|' ) )
+	echo -n "FILNAMN: "
 	echo $FILNAMN
-	touch $CWD/completed/completed_$FILNAMN
+	touch $CWD/completed/completed_${FILNAMN}
 #	cut -f1 -d'     ' header > $CWD/completed/completed_$header
 	sleep 1
 done
@@ -76,14 +82,18 @@ set -- $FINALFILES
 INCREMENTER=1
 
 # This loop pastes the species headers into the files
+echo "sequence paste loop"
 for tf in $TABBEDFILES
 do
+	echo -n "dollar 1 in haders paste loop: "
 	echo $1
-	cut -f1 -d'	' tf >> $CWD/completed/completed_$(${INCREMENTER})
+	echo -n "INCREMENTER: "
+	echo $INCREMENTER
+	cut -f1 -d'	' $CWD/tabbed/$tf >> $CWD/completed/completed_${"$INCREMENTER"}
 #	This loop pastes the sequences into the result files	
 	for seq in $TABBEDFILES
 	do
-		cut -f2 -d'	' seq >> $CWD/completed/completed_$($INCREMENTER)
+		cut -f2 -d'	' $CWD/tabbed/$seq >> $CWD/completed/completed_$($INCREMENTER)
 	done
 	INCREMENTER=$((INCREMENTER+1))
 	sleep 1
